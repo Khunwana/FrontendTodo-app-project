e45e5448f6909ccfc99483aee37494d623226498
Monias <khunwana.monias@gmail.com>
2024-05-17 23:27:37 +0200

    restructuring App.js
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import { Component } from 'react';
+
 import './App.css';
+import ComponentImports  from './components/templating/ComponentImports';
+
 
 function App() {
   return (
     <div className="App">
-      
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-      <FourthComponent />
+      <ComponentImports />
     </div>
   );
 }
-
-function FirstComponent()
-{
-  return (
-    <div className='FirstComponent'>First Component</div>
-  )
-}
-
-function SecondComponent()
-{
-  return (
-    <div className='SecondComponent'>Second Component</div>
-  )
-}
-
-class ThirdComponent extends Component
-{
-  render()
-  {
-    return (
-      <div className='ThirdComponent'>Third Component</div>
-    )
-  }
-}
-
-class FourthComponent extends Component
-{
-  render()
-  {
-    return (
-      <div className='FourthComponent'>Fourth Component</div>
-    )
-  }
-}
-
 export default App;
