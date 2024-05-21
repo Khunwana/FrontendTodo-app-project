92e363945539114840164b3e8836ce237a3a5f0f
Monias <khunwana.monias@gmail.com>
2024-05-21 18:42:05 +0200

    code refactoring App.js
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
 import './App.css';
-// import ComponentImports  from './components/templating/ComponentImports';
 import Counter  from './components/counter/Counter';
-
 
 function App() {
   return (
     <div className="App">
-      {/* <ComponentImports /> */}
       <Counter />
     </div>
   );
 }
+
 export default App;
