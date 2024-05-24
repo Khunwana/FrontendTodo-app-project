fc2880f8e86ee020c07193f33618c6a93bc8acd4
Monias <khunwana.monias@gmail.com>
2024-05-24 23:09:22 +0200

    code cleanup
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/security/AuthContext.js
+++ src/components/to_do/security/AuthContext.js
@@ -6,16 +6,11 @@ export const useAuth = () => useContext(AuthContext)
 
 export default function AuthProvider({children})
 {
-    const [number,setNumber]=useState(0)
-
     //cheking if user is logged in
     const [isAuthenticated,setAuthenticated] = useState(false)
 
-    //checking if number state is being updated 
-    
-    // setInterval( () => setNumber(number+1),10000)
     return (
-        <AuthContext.Provider value={{number,isAuthenticated,setAuthenticated}}>
+        <AuthContext.Provider value={{isAuthenticated,setAuthenticated}}>
             {children}
         </AuthContext.Provider>
     )
