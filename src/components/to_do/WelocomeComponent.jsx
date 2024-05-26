08324232b817b8ad672c3b966c004a82abb73c62
Monias <khunwana.monias@gmail.com>
2024-05-26 23:47:21 +0200

    calling ResT API
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/WelocomeComponent.jsx
+++ src/components/to_do/WelocomeComponent.jsx
@@ -1,15 +1,40 @@
 import { useParams, Link } from 'react-router-dom'
-
+import axios from 'axios'
 export default function WelocomeComponent()
 {
     const {username} = useParams()
     console.log(username)
+    function callhelloworldRestApi()
+    {
+        console.log("callled")
+        //axios
+        axios.get('http://localhost:8080/hello-world')
+            .then( (respone) => succesfulResponse(respone))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log("clean up"))
+    }
+
+    function succesfulResponse(response)
+    {
+        console.log(response)
+    }
+
+    function errorResponse(error)
+    {
+        console.log(error)
+    }
+
     return (
         <div className="Welcome">
 
         <h1>Welcome {username}</h1>
         <div >
             Manage What To-Do - <Link to="/listToDo">Go here</Link>
+        </div>
+        <div>
+            <button className="btn btn-success m-5" onClick={callhelloworldRestApi}>
+                hello world Rest API
+                </button>
         </div>
         </div>
     )
