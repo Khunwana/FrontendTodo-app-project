0687244223aedd2db51753dce7a3e68a8e71e412
Monias <khunwana.monias@gmail.com>
2024-05-30 03:55:51 +0200

    call and upate table with data from Api call
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/ListToDoComponent.jsx
+++ src/components/to_do/ListToDoComponent.jsx
@@ -1,22 +1,62 @@
+import { useEffect, useState } from "react"
+import { retrieveAllToDoForusername,deleteTo_Do } from "./api/ListToDoApiService"
+import { useParams } from "react-router-dom"
+import { useAuth } from "./security/AuthContext"
+
+
 export default function ListToDoComponent()
 {
-    const today = new Date()
-    const targetDate = new Date(today.getFullYear(),today.getMonth(),today.getDay())
+    const [toDo,setTo_Do] = useState([])
+    const [message,setmessage] = useState("")
+    const refreshTime = 2000
+    const authContext = useAuth()
+    const username = authContext.username
+   
+    useEffect(
+        () => resfreshTo_Do()
+    )
 
-    const toDo = [  {id:1,description:'Learn full-stack', done : false, targetDate :targetDate },
-                    {id:2,description:'Learn nodeJs', done : false, targetDate : targetDate},
-                    {id:3,description:'Learn flutter', done : false, targetDate :targetDate }]
+    function resfreshTo_Do()
+    {
+        retrieveAllToDoForusername(username)
+            .then(response => setTo_Do(response.data,refreshTime),
+            
+            )
+            .catch(error => console.log(error))
+            
+    }
+
+
+    function deleteToDo(id)
+    {
+        console.log("deleted" + id)
+        deleteTo_Do(id,username)
+            .then(
+                //Display message
+                setmessage(`Deleted To-Do ${id}`),
+                //update To-Do table
+                () => resfreshTo_Do()
+            )
+    }
+
+    function createToDo()
+    {
+        console.log("Create To-Do function")
+    }
+
     return (
         <div className="container">
             <h1>Things You Want To Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div >
             <table className='table'>
                 <thead>
                     <tr>
-                        <td>id</td>
-                        <td>description</td>
-                        <td>done</td>
-                        <td>targetDate</td>
+                        <th>description</th>
+                        <th>done</th>
+                        <th>targetDate</th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,15 +64,15 @@ export default function ListToDoComponent()
                         toDo.map(
                             todoList => (
                                 <tr key={todoList.id}>
-                                    <td>{todoList.id}</td>
                                     <td>{todoList.description}</td>
                                     <td>{todoList.done.toString()}</td>
-                                    <td>{todoList.targetDate.toDateString()}</td>
+                                    <td>{todoList.targetDate.toString()}</td>
+                                    <td><button className="btn btn-warning" onClick={() => deleteToDo(todoList.id)}>Delete</button></td>
+                                    <td><button className="btn btn-success" onClick={() => createToDo(todoList.id)}>Create</button></td>
                                 </tr> 
                             )
                         )
-                    }
-                                   
+                    }                                   
                 </tbody>
             </table>
             </div>
