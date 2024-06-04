440da264213999e53993f3972075fcd837673780
Monias <khunwana.monias@gmail.com>
2024-06-04 17:28:39 +0200

    repacling headers call with headers calling token
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/api/HelloWorldApiService.jsx
+++ src/components/to_do/api/HelloWorldApiService.jsx
@@ -4,19 +4,34 @@ const apiClient = axios.create({
     baseURL : 'http://localhost:8080'
     
 })
-export default function GethelloworldBean()
-{
-    return apiClient.get('/hello-world',{
-            auth:{
-                username: 'username',
-                password: 'password'
-            },
-            headers:{
-                'Content-Type' : 'application/json'
-            }
+// export default function GethelloworldBean()
+// {
+//     return apiClient.get('/hello-world',{
+//             auth:{
+//                 username: 'username',
+//                 password: 'password'
+//             },
+//             headers:{
+//                 'Content-Type' : 'application/json'
+//             }
 
-         })
-}
+//          })
+// }
+
+export const  GethelloworldBean
+    = (username) => apiClient.get('/hello-world',{
+        headers : {
+            Authorization: 'Basic dXNlcm5hbWU6cGFzc3dvcmQ='
+        }
+})
+
+//tesing basic authentication to remove hardcoded authentication
+export const  executeBasicAuth
+    = (token) => apiClient.get('/basicAuth',{
+        headers : {
+            Authorization: token
+        }
+})
 
 export const getHelloWorldBeanPathVariable 
     = (username) => apiClient.get(`/hello-world/path-variable/${username}`)
