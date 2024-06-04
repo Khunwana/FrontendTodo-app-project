2617b696a8220abc3880a794459ce62fed763281
Monias <khunwana.monias@gmail.com>
2024-06-04 17:33:09 +0200

    refactoring and using async and await to wait for response from api
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/security/AuthContext.js
+++ src/components/to_do/security/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { executeBasicAuth } from "../api/HelloWorldApiService";
 
 export const AuthContext = createContext()
 
@@ -9,10 +10,19 @@ export default function AuthProvider({children})
     //cheking if user is logged in
     const [isAuthenticated,setAuthenticated] = useState(false)
     const [username,setUsername] = useState("")
+    const [success,setSuccess] = useState()
 
-    function login(username,password)
+    function testing(response)
     {
-        if( username === 'Khunwana' && password === '1234')
+        console.log(response)
+    }
+
+    async function login(username,password)
+    {
+        const basicAuthToken = 'Basic '+ window.btoa(username + ":" + password)
+        const response = await executeBasicAuth(basicAuthToken)
+       
+        if( response.status == 200)
             {
                 setAuthenticated(true)
                 setUsername(username)
@@ -23,6 +33,7 @@ export default function AuthProvider({children})
                 setAuthenticated(false)
                 return false
             }
+        setAuthenticated(false)
     }
 
     function logout()
