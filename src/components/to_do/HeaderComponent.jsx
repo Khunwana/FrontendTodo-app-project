4997a3f7fa6e16a47fc39b840f4d139a745c6ffe
Monias <khunwana.monias@gmail.com>
2024-08-10 23:30:00 +0200

    removing unused code
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/HeaderComponent.jsx
+++ src/components/to_do/HeaderComponent.jsx
@@ -25,28 +25,7 @@ export default function HeaderComponent()
         authContext.logout()
     }
     return (
-        // <header className="border-bottom border-light border-5  p-2">
-        //     <div className="container">
-        //         <div className="row">
-        //             <nav className="navbar navbar-fixed navbar-expand-lg" fixed="top">
-        //                 <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https//www.khunwana.co.za">Khunwana</a>
-        //                 <div className="collapse navbar-collapse">
-        //                     <ul className="navbar-nav">
-        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>}
-        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/welcome/:username">Welcome</Link></li>}
-        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/listTodo">To-Do</Link></li>}
-        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/home">home</Link></li>}
-        //                     </ul>
-        //                 </div>
-        //                 <ul className="navbar-nav">
-        //                     {!isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li> }   
-        //                     {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/logout" onClick={logout}>Logout</Link></li>}
-                            
-        //                 </ul>
-        //             </nav>
-        //         </div>
-        //     </div>
-        // </header>
+      
         <nav className="navbar fixed-top border-bottom border-light border-5  p-2">
         {/* <div className="container"> */}
           <div className="logo">
