86c73fbfef0896dcad813a0b4831834c13736701
Monias <khunwana.monias@gmail.com>
2024-08-10 23:34:24 +0200

    changing baseUrl to amazon hosted restApi
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/api/ApiClient.js
+++ src/components/to_do/api/ApiClient.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL : 'http://localhost:8080'    
+    // baseURL : 'http://localhost:8080' 
+    baseURL : 'http://please-tezting-rest-env.eba-qs3rjarg.eu-north-1.elasticbeanstalk.com/'   
 })
