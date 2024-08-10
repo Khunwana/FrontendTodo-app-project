f1f3aae52b9c9ccf65eff2cdd8f34999550c0539
Monias <khunwana.monias@gmail.com>
2024-08-10 23:07:29 +0200

    changed navigation bar
    Signed-off-by: Monias <khunwana.monias@gmail.com>

--- src/components/to_do/HeaderComponent.jsx
+++ src/components/to_do/HeaderComponent.jsx
@@ -1,37 +1,88 @@
 import { Link } from 'react-router-dom'
 import {  useAuth } from './security/AuthContext'
-
+import '../profile/assets/css/style.css'
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 export default function HeaderComponent()
 {
     const authContext = useAuth()
     const isAuthenticated = authContext.isAuthenticated
+
+    const [showNav, setShowNav] = useState(false)
+
+    const toggleNavItems = () => {
+    setShowNav(!showNav)
+    }
+
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
 
     function logout()
     {
         authContext.logout()
     }
     return (
-        <header className="border-bottom border-light border-5  p-2">
-            <div className="container">
-                <div className="row">
-                    <nav className="navbar navbar-expand-lg">
-                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https//www.khunwana.co.za">Khunwana</a>
-                        <div className="collapse navbar-collapse">
-                            <ul className="navbar-nav">
-                                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>}
-                                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/welcome/:username">Welcome</Link></li>}
-                                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/listTodo">To-Do</Link></li>}
-                                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/home">home</Link></li>}
-                            </ul>
-                        </div>
-                        <ul className="navbar-nav">
-                            {!isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li> }   
-                            {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/logout" onClick={logout}>Logout</Link></li>}
+        // <header className="border-bottom border-light border-5  p-2">
+        //     <div className="container">
+        //         <div className="row">
+        //             <nav className="navbar navbar-fixed navbar-expand-lg" fixed="top">
+        //                 <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https//www.khunwana.co.za">Khunwana</a>
+        //                 <div className="collapse navbar-collapse">
+        //                     <ul className="navbar-nav">
+        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>}
+        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/welcome/:username">Welcome</Link></li>}
+        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/listTodo">To-Do</Link></li>}
+        //                         {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/home">home</Link></li>}
+        //                     </ul>
+        //                 </div>
+        //                 <ul className="navbar-nav">
+        //                     {!isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li> }   
+        //                     {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/logout" onClick={logout}>Logout</Link></li>}
                             
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
+        //                 </ul>
+        //             </nav>
+        //         </div>
+        //     </div>
+        // </header>
+        <nav className="navbar fixed-top border-bottom border-light border-5  p-2">
+        {/* <div className="container"> */}
+          <div className="logo">
+            <h1>Khunwana</h1>
+          </div>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+          {/* <Hamburger /> */}
+          <h1>=</h1>
+          </div>
+          <div className={`nav-elements  ${showNavbar && 'active'}`}>
+            <ul>
+              <li>
+                {/* <NavLink to="/">Home</NavLink> */}
+                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/home">Home</Link></li>}
+              </li>
+              <li>
+                {/* <NavLink to="/blog">Blog</NavLink> */}
+                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/welcome/:username">Welcome</Link></li>}
+              </li>
+              <li>
+                {/* <NavLink to="/projects">Projects</NavLink> */}
+                {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/listTodo">To-Do</Link></li>}
+              </li>
+              <li>
+                {/* <NavLink to="/about">About</NavLink> */}
+                {/* {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/">home</Link></li>} */}
+              </li>
+              <li>
+                {/* <NavLink to="/contact">Contact</NavLink> */}
+                {!isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li> }   
+                 {isAuthenticated && <li className="nav-item"><Link className="nav-link" to="/logout" onClick={logout}>Logout</Link></li>}
+              </li>
+            </ul>
+          </div>
+        {/* </div> */}
+      </nav>
+       
     )
 }
